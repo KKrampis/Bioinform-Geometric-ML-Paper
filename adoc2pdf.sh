c76b06fed96183a38d11738226c749f5a91bdea0
Ntino <agbiotec@gmail.com>
2023-04-20 13:30:22 +0200

    half polished, formulas fixed, figure legend, need references

--- adoc2pdf.sh
+++ adoc2pdf.sh
@@ -1,2 +1,6 @@
-asciidoctor -r asciidoctor-bibtex -b docbook GeoDeepLearnBio.adoc
+#makes html
+asciidoctor -v -r asciidoctor-diagram -r asciidoctor-bibtex GeoDeepLearnBio.adoc
+#makes docbook xml (as intermediate for pdf conversion)
+asciidoctor -v -r asciidoctor-diagram -r asciidoctor-bibtex -b docbook GeoDeepLearnBio.adoc
+#pandoc used to convert docbook to pdf
 pandoc -f docbook GeoDeepLearnBio.xml -o GeoDeepLearnBio.pdf
