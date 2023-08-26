57a240ea00b313c6639220426c9529df03605575
Ntino <agbiotec@gmail.com>
2023-08-26 20:12:40 +0200

    final commit for submission

--- adoc2pdf.sh
+++ adoc2pdf.sh
@@ -1,8 +1,8 @@
 #makes html
-asciidoctor -v -r asciidoctor-diagram -r asciidoctor-bibtex GeoDeepLearnBio.adoc
+asciidoctor -v -r asciidoctor-diagram -r asciidoctor-bibtex GeoDeepLearnBio_v2.adoc
 #makes docbook xml (as intermediate for pdf conversion)
-asciidoctor -v -r asciidoctor-diagram -r asciidoctor-bibtex -b docbook GeoDeepLearnBio.adoc
+asciidoctor -v -r asciidoctor-diagram -r asciidoctor-bibtex -b docbook GeoDeepLearnBio_v2.adoc
 #pandoc used to convert docbook to pdf
-pandoc -V geometry:margin=1.15in -f docbook GeoDeepLearnBio.xml -o GeoDeepLearnBio.pdf
+pandoc -V geometry:margin=1.15in -f docbook GeoDeepLearnBio_v2.xml -o GeoDeepLearnBio_v2.pdf
 #pandoc used to convert docbook to doc
-pandoc -f docbook GeoDeepLearnBio.xml -o GeoDeepLearnBio.docx
+pandoc -f docbook GeoDeepLearnBio_v2.xml -o GeoDeepLearnBio_v2.docx
